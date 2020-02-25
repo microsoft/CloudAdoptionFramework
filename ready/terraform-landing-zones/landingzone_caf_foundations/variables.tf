6fcc022cc9c013d1efc7f566c34f07f27b15d39e
Arnaud Lheureux <arnaudlh@hotmail.com>
2020-02-25 13:43:55 +0800

    Update to redirect to new repo

--- ready/terraform-landing-zones/landingzone_caf_foundations/variables.tf
+++
@@ -1,5 +0,0 @@
-# Map of the remote data state
-variable "lowerlevel_storage_account_name" {}
-variable "lowerlevel_container_name" {}
-variable "lowerlevel_key" {}                  # Keeping the key for the lower level0 access
-variable "lowerlevel_resource_group_name" {}
