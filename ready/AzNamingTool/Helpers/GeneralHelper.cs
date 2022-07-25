044926994ec97ec25cb7d7f0db5359a23166314d
BryanSoltis <github@soltisweb.com>
2022-07-25 07:43:57 -0500

    Azure Naming Tool #70 - Fix issue #70 - Handle resource types without min/max length defined

--- ready/AzNamingTool/Helpers/GeneralHelper.cs
+++ ready/AzNamingTool/Helpers/GeneralHelper.cs
@@ -99,8 +99,9 @@ namespace AzureNamingTool.Helpers
 
                 return items;
             }
-            catch
+            catch (Exception ex)
             {
+                GeneralHelper.LogAdminMessage("ERROR", ex.Message);
                 throw;
             }
         }
@@ -142,22 +143,23 @@ namespace AzureNamingTool.Helpers
                         break;
                 }
             }
-            catch
+            catch (Exception ex)
             {
+                GeneralHelper.LogAdminMessage("ERROR", ex.Message);
                 throw;
             }
         }
 
         public static bool CheckNumeric(string value)
         {
-            Regex regx = new Regex("^[0-9]+$");
+            Regex regx = new("^[0-9]+$");
             Match match = regx.Match(value);
             return match.Success;
         }
 
         public static bool CheckAlphanumeric(string value)
         {
-            Regex regx = new Regex("^[a-zA-Z0-9]+$");
+            Regex regx = new("^[a-zA-Z0-9]+$");
             Match match = regx.Match(value);
             return match.Success;
         }
@@ -183,17 +185,13 @@ namespace AzureNamingTool.Helpers
                 aes.Key = Encoding.UTF8.GetBytes(keyString);
                 aes.IV = iv;
                 ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-                using (MemoryStream memoryStream = new MemoryStream())
+                using MemoryStream memoryStream = new();
+                using CryptoStream cryptoStream = new((Stream)memoryStream, encryptor, CryptoStreamMode.Write);
+                using (StreamWriter streamWriter = new((Stream)cryptoStream))
                 {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
-                        {
-                            streamWriter.Write(text);
-                        }
-                        array = memoryStream.ToArray();
-                    }
+                    streamWriter.Write(text);
                 }
+                array = memoryStream.ToArray();
             }
             return Convert.ToBase64String(array);
         }
@@ -202,23 +200,15 @@ namespace AzureNamingTool.Helpers
         {
             byte[] iv = new byte[16];
             byte[] buffer = Convert.FromBase64String(cipherText);
-            using (Aes aes = Aes.Create())
-            {
-                aes.KeySize = 256;
-                aes.Key = Encoding.UTF8.GetBytes(keyString);
-                aes.IV = iv;
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-                using (MemoryStream memoryStream = new MemoryStream(buffer))
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
-                        {
-                            return streamReader.ReadToEnd();
-                        }
-                    }
-                }
-            }
+            using Aes aes = Aes.Create();
+            aes.KeySize = 256;
+            aes.Key = Encoding.UTF8.GetBytes(keyString);
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using MemoryStream memoryStream = new(buffer);
+            using CryptoStream cryptoStream = new((Stream)memoryStream, decryptor, CryptoStreamMode.Read);
+            using StreamReader streamReader = new((Stream)cryptoStream);
+            return streamReader.ReadToEnd();
         }
 
         public static void VerifyConfiguration()
@@ -226,7 +216,7 @@ namespace AzureNamingTool.Helpers
             try
             {
                 // Get all the files in teh repository folder
-                DirectoryInfo dirRepository = new DirectoryInfo("repository");
+                DirectoryInfo dirRepository = new("repository");
                 foreach (FileInfo file in dirRepository.GetFiles())
                 {
                     // Check if the file exists in the settings folder
@@ -254,7 +244,7 @@ namespace AzureNamingTool.Helpers
                     {
                         // Create a new SALT key 
                         const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-                        Random random = new Random();
+                        Random random = new();
                         var salt = new string(Enumerable.Repeat(chars, 16)
                             .Select(s => s[random.Next(s.Length)]).ToArray());
 
@@ -390,30 +380,36 @@ namespace AzureNamingTool.Helpers
                 }
 
                 // Check min length
-                if (name.Length < int.Parse(resourceType.LengthMin))
+                if (int.TryParse(resourceType.LengthMin, out _))
                 {
-                    sbMessage.Append("Generated name is less than the minimum length for the selected resource type.");
-                    sbMessage.Append(Environment.NewLine);
-                    valid = false;
-                }
-
-                // Check max length
-                if (name.Length > int.Parse(resourceType.LengthMax))
-                {
-                    // Strip the delimiter in case that is causing the issue
-                    name = name.Replace(delimiter, "");
-                    if (name.Length > int.Parse(resourceType.LengthMax))
+                    if (name.Length < int.Parse(resourceType.LengthMin))
                     {
-                        sbMessage.Append("Generated name is more than the maximum length for the selected resource type.");
-                        sbMessage.Append(Environment.NewLine);
-                        sbMessage.Append("Please remove any optional components or contact your admin to update the required components for this resource type.");
+                        sbMessage.Append("Generated name is less than the minimum length for the selected resource type.");
                         sbMessage.Append(Environment.NewLine);
                         valid = false;
                     }
-                    else
+                }
+
+                // Check max length
+                if (int.TryParse(resourceType.LengthMax, out _))
+                {
+                    if (name.Length > int.Parse(resourceType.LengthMax))
                     {
-                        sbMessage.Append("Generated name with the selected delimiter is more than the maximum length for the selected resource type. The delimiter has been removed.");
-                        sbMessage.Append(Environment.NewLine);
+                        // Strip the delimiter in case that is causing the issue
+                        name = name.Replace(delimiter, "");
+                        if (name.Length > int.Parse(resourceType.LengthMax))
+                        {
+                            sbMessage.Append("Generated name is more than the maximum length for the selected resource type.");
+                            sbMessage.Append(Environment.NewLine);
+                            sbMessage.Append("Please remove any optional components or contact your admin to update the required components for this resource type.");
+                            sbMessage.Append(Environment.NewLine);
+                            valid = false;
+                        }
+                        else
+                        {
+                            sbMessage.Append("Generated name with the selected delimiter is more than the maximum length for the selected resource type. The delimiter has been removed.");
+                            sbMessage.Append(Environment.NewLine);
+                        }
                     }
                 }
 
@@ -510,8 +506,9 @@ namespace AzureNamingTool.Helpers
                 };
                 lstAdminLogMessages = JsonSerializer.Deserialize<List<AdminLogMessage>>(data, options).OrderByDescending(x => x.CreatedOn).ToList();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                GeneralHelper.LogAdminMessage("ERROR", ex.Message);
             }
             return lstAdminLogMessages;
         }
@@ -520,7 +517,7 @@ namespace AzureNamingTool.Helpers
         {
             try
             {
-                AdminLogMessage adminmessage = new AdminLogMessage()
+                AdminLogMessage adminmessage = new()
                 {
                     Id = 1,
                     CreatedOn = DateTime.Now,
@@ -552,10 +549,26 @@ namespace AzureNamingTool.Helpers
             {
                 await FileSystemHelper.WriteFile("adminlog.json", "[]");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
+                GeneralHelper.LogAdminMessage("ERROR", ex.Message);
             }
+        }
+
+        public static async Task<string> DownloadString(string url)
+        {
+            string data;
+            try
+            {
+                HttpClient httpClient = new();
+                data = await httpClient.GetStringAsync(url);
+            }
+            catch (Exception ex)
+            {
+                GeneralHelper.LogAdminMessage("ERROR", ex.Message);
+                data = "";
+            }
+            return data;
         }
     }
 }
