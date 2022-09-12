bb483ecab57126a7984c170c26547d562cb5ad0d
BryanSoltis <bryan@soltisweb.com>
2022-09-12 14:32:47 -0500

    2022-09-12 BryanSoltis - Created AdminServices class. Updated AdminModal to use service. Created AdminController for API to use service.

---
+++ ready/AzNamingTool/Controllers/AdminController.cs
@@ -0,0 +1,116 @@
+﻿using AzureNamingTool.Models;
+using AzureNamingTool.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AzureNamingTool.Services;
+using AzureNamingTool.Attributes;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureNamingTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiKey]
+    public class AdminController : ControllerBase
+    {
+        private ServiceResponse serviceResponse = new();
+        private Config config = GeneralHelper.GetConfigurationData();
+
+        // POST api/<AdminController>
+        /// <summary>
+        /// THis function will update the admin password. 
+        /// </summary>
+        /// <param name="newpassword">string - New admin password.</param>
+        /// <returns>bool - Successful update</returns>
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> UpdatePassword([FromBody] string password)
+        {
+            try
+            {
+                serviceResponse = await AdminService.UpdatePassword(password);
+                if (serviceResponse.Success)
+                {
+                    return Ok("SUCCESS");
+                }
+                else
+                {
+                    return Ok("FAILURE - There was a problem updating the password.");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogAdminMessage("ERROR", ex.Message);
+                return BadRequest(ex);
+            }
+        }
+
+        // POST api/<AdminController>
+        /// <summary>
+        /// THis function will update the admin password. 
+        /// </summary>
+        /// <param name="newpassword">string - New API Key.</param>
+        /// <returns>bool - Successful update</returns>
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> UpdateAPIKey([FromBody] string apikey)
+        {
+            try
+            {
+                serviceResponse = await AdminService.UpdateAPIKey(apikey);
+                if (serviceResponse.Success)
+                {
+                    return Ok("SUCCESS");
+                }
+                else
+                {
+                    return Ok("FAILURE - There was a problem updating the API Key.");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogAdminMessage("ERROR", ex.Message);
+                return BadRequest(ex);
+            }
+        }
+
+        // POST api/<AdminController>
+        /// <summary>
+        /// THis function will update the admin password. 
+        /// </summary>
+        /// <param name="newpassword">string - New API Key.</param>
+        /// <returns>string - Successful update</returns>
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> GenerateAPIKey()
+        {
+            try
+            {
+                serviceResponse = await AdminService.GenerateAPIKey();
+                if (serviceResponse.Success)
+                {
+                    return Ok("SUCCESS - API Key: " + serviceResponse.ResponseObject);
+                }
+                else
+                {
+                    return Ok("FAILURE - There was a problem generating the API Key.");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogAdminMessage("ERROR", ex.Message);
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
