96a2f703a32884c13981b865d16017ae8a01f873
Bryan Soltis <github@soltisweb.com>
2023-07-31 13:38:11 -0500

    2023-07-31 BryanSoltis - v3.0.0 Updates - Removed all Azure Naming Tools Files - Updated README to direct users to new AzureNamingTool repo - Updated tool version in configruation files

--- ready/AzNamingTool/Models/ConfigurationFileVersionData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AzureNamingTool.Models
-{
-    public class ConfigurationFileVersionData
-    {
-        public string resourcetypes { get; set; } = "0.0.0";
-        public string resourcelocations { get; set; } = "0.0.0";
-    }
-}
--- ready/AzNamingTool/Models/CustomComponmentConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AzureNamingTool.Models
-{
-    public class CustomComponmentConfig
-    {
-        public List<ResourceComponent>? ParentComponents { get; set; }
-        public List<CustomComponent>? CustomComponents { get; set; }
-    }
-}
